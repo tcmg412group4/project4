a0dbc32035ef71ca96d2f8b9c16f19fec962f74d
Austin John <austinjohn@tamu.edu>
2023-02-08 15:35:21 -0600

    monthlylogcreator 2

--- monthlylogcreator.py
+++ monthlylogcreator.py
@@ -3,13 +3,13 @@ import re
 entirelog = open('EntireLog.txt', 'r')
 
 log = entirelog.read()
-logAslist = log.splitlines()
-print(logAslist[1]) #this is a test to see if the log becomes a list properly
+logAslist = entirelog.readlines()
+#print(logAslist[1]) #this is a test to see if the log becomes a list properly
 
-pattern1 = "?:0?[1-9]|1\d|2[0-8])(\/|-|\.)(?:(?:0?[1-9]|(?:Jan|Feb|Mar|Apr|May|Jun|Jul|Aug|Sep))|(?:1[0-2]|(?:Oct|Nov|Dec)))\4(?:(?:1[6-9]|[2-9]\d)?\d{2})$"
+pattern1 = "([\d]{2}\s(Jan|Feb|Mar|Apr|May|Jun|Jul|Aug|Sep|Oct|Nov|Dec)\s[\d]{4})" #search pattern specified to date format on the log
 
 for i in logAslist:
-    if(re.search(pattern1, i)):
+    if(re.search(pattern1, i) != None):
         print("Date found")
     else:
         print("No date found")
