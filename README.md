b7531fcca056f9f0a8893ce77e42a9c03fd4b2ae
Austin John <austinjohn@tamu.edu>
2023-02-07 18:52:38 -0600

    first commit

--- README.md
+++ README.md
@@ -1,1 +1,4 @@
 # project4
+
+This is Group 4's repository for TCMG 412 group project #4. Below includes packages and steps to run the files in this repository to get the desired outcome outlined
+on Canvas. 
---
+++ import.py
@@ -0,0 +1,13 @@
+import urllib.request
+
+textfile = urllib.request.urlopen("https://s3.amazonaws.com/tcmg476/http_access_log")
+
+data = open('EntireLog.txt','w+')
+
+for line in textfile:
+    strline = str(line)
+    data.write(strline)
+    data.write('\n')
+
+data.close()
+
