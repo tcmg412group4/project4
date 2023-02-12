aee648a4df9ca992a5ce9de7c8757a7e0e41e5dd
mariasuchil <mariasuchil_17@tamu.edu>
2023-02-11 18:26:08 -0600

    fixed my variables

--- maria.py
+++ maria.py
@@ -6,7 +6,9 @@ all94 = 0
 all95 = 0
 bad = 0
 good = 0
+seq = []
 
+pattern = re.compile("[0-9]{2}[^a-zA-Z0-9]{1}[a-zA-Z]{3}[^a-zA-Z0-9]{1}[0-9]{4}?") 
 
 
 # open file again, name it data 
@@ -18,12 +20,16 @@ with open('EntireLog.txt') as data:
             bad += 1
         if '302 -'in x:
             good += 1
+        #seq = re.split(".+\[(.+)\]*\"(.+)\"*(\d{3})", x)
     dec = good / allreq
     perc = "{:.0%}".format(dec)
     decc = (bad / allreq)
     percent = "{:.0%}".format(decc)
 
-print("unsuccessful requests = ",percent)
-print("redirected requests = ",perc)
+print("unsuccessful requests = ",bad)
+print("redirected requests = ",good)
+print(pattern)
+
+
 
 # I feel like my numbers are wrong :( but the code works
