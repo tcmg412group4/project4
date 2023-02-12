e0bda9a61ea4f54570f970d89d15ab49aedbad8e
mariasuchil <mariasuchil_17@tamu.edu>
2023-02-12 17:48:01 -0600

    maria code

--- maria.py
+++ maria.py
@@ -6,29 +6,35 @@ all94 = 0
 all95 = 0
 bad = 0
 good = 0
-seq = []
-
-pattern = re.compile("[0-9]{2}[^a-zA-Z0-9]{1}[a-zA-Z]{3}[^a-zA-Z0-9]{1}[0-9]{4}?") 
 
 
 # open file again, name it data 
 with open('EntireLog.txt') as data:
     L = data.readlines()
+#seq = re.split(".+\[(.+)\.+\]*\"(.+)\"*(\d{3})", x)
     for x in L:
         allreq += 1
         if '403 -' in x or '404 -' in x:
             bad += 1
         if '302 -'in x:
             good += 1
-        #seq = re.split(".+\[(.+)\]*\"(.+)\"*(\d{3})", x)
     dec = good / allreq
     perc = "{:.0%}".format(dec)
     decc = (bad / allreq)
     perc2 = "{:.0%}".format(decc)
+    
+    seq = re.split(".+\[(.+).+]*\"(A-Z)*(.+) (.+)\" ([0-9]{3})", x)
+    file = seq[2]
+    fname = {}
+    if file in fname:
+        fname[file] += 1
+    else:
+        fname[file] = 1
+    
 
 print("unsuccessful requests = ",perc)
 print("redirected requests = ",perc2)
-print(pattern)
+# print("Least requested: " + str(list(fname.keys())[-1]))
 
 
 
