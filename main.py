b3c07b5d02a6550c0d8d2f9a81b9f11317f1688c
Austin John <austinjohn@tamu.edu>
2023-02-12 18:59:30 -0600

    splits log into 12 files after being run

--- main.py
+++ main.py
@@ -8,7 +8,7 @@ data = open('EntireLog.txt', 'r')                                               
 log = data.readlines()                                                                          # reads the entire log file line by line
 matches = [] 
 
-
+                                                                                                # create empty lists to hold daily logs
 oct94 = []
 nov = []
 dec = []
@@ -23,6 +23,22 @@ aug = []
 sep = []
 oct95 = []
 errorlog = []
+                                                                                                # create text files for individual months
+october94 = open("October94.txt", "w+")
+november = open("November.txt", "w+")
+december = open("December.txt", "w+")
+january = open("January.txt", "w+")
+february = open("February.txt", "w+")
+march = open("March.txt", "w+")
+april = open("April.txt", "w+")
+May = open("May.txt", "w+")
+june = open("June.txt", "w+")
+july = open("July.txt", "w+")
+august = open("August.txt", "w+")
+september = open("September.txt", "w+")
+october95 = open("October95", "w+")
+errorLog = open("ErrorLog.txt", "w+")
+
 
 for line in log:                                                                                # for loop for parsing through text file line by line 
     x = re.search(pattern, line)                                                                # searches each line for the Date pattern
@@ -38,38 +54,49 @@ for i in matches:
 
     if i.find('Oct/1994') != -1 :
         oct94.append(i)
+        october94.write(i + '\n')
     elif i.find('Nov/1994') != -1 :
         nov.append(i)
+        november.write(i + '\n')
     elif i.find("Dec/1994") != -1 :
         dec.append(i)
+        december.write(i + '\n')
     elif i.find("Jan/1995") != -1 :
         jan.append(i)
+        january.write(i + '\n')
     elif i.find("Feb/1995") != -1 :
         feb.append(i)
+        february.write(i + '\n')
     elif i.find("Mar/1995") != -1 :
         mar.append(i)
+        march.write(i + '\n')
     elif i.find("Apr/1995") != -1 :
         apr.append(i)
+        april.write(i + '\n')
     elif i.find("May/1995") != -1 :
         may.append(i)
+        May.write(i + '\n')
     elif i.find("Jun/1995") != -1 :
         jun.append(i)
+        june.write(i + '\n')
     elif i.find("Jul/1995") != -1 :
         jul.append(i)
+        july.write(i + '\n')
     elif i.find("Aug/1995") != -1 :
         aug.append(i)
+        august.write(i + '\n')
     elif i.find("Sep/1995") != -1 :
         sep.append(i)
+        september.write(i + '\n')
     elif i.find("Oct/1995") != -1 :
         oct95.append(i)
+        october95.write(i + '\n')
     elif i.find("local 780") != -1 :                    #not sure how to append error log to proper month lists
         errorlog.append(i)
+        errorLog.write(i + '\n')
     elif i.find("local      index.html") != -1 :
         errorlog.append(i)
-
-    
-
-
-
-
+        errorLog.write(i + '\n')
+                                                        # program works, splits logs into monthly files after being run, all except the error logs. Not sure
+                                                        # about how to add the error logs to the proper month when the program is parsing through the entire log file. 
 
