53b4a6644179337b155698d5121d945b90ef461c
mariasuchil <mariasuchil_17@tamu.edu>
2023-02-13 17:28:27 -0600

    solves questions 3,4,5,6

--- maria.py
+++ maria.py
@@ -1,41 +1,43 @@
 import re
+import operator
 
 # some variables
 allreq = 0
-all94 = 0
-all95 = 0
 bad = 0
 good = 0
+records = {}
 
 
 # open file again, name it data 
 with open('EntireLog.txt') as data:
-    L = data.readlines()
-#seq = re.split(".+\[(.+)\.+\]*\"(.+)\"*(\d{3})", x)
+    L = data.readlines()                
     for x in L:
         allreq += 1
-        if '403 -' in x or '404 -' in x:
+        if '403 -' in x or '404 -' in x:                #search for errors
             bad += 1
-        if '302 -'in x:
+        if '302 -'in x:                                 #search for redirects
             good += 1
+        seq = re.split("[A-Z]{3,5} (.+) HTTP/1.0", x)           #pattern to filter out the name of the file only
+        if len(seq) == 3:
+            file = seq[1]                               #index
+            if file in records:                         #count and store it in a dictionary
+                records[file] += 1
+            else:
+                records[file] = 1
+
     dec = good / allreq
-    perc = "{:.0%}".format(dec)
+    perc = "{:.0%}".format(dec)                         #convert to percent
     decc = (bad / allreq)
-    perc2 = "{:.0%}".format(decc)
-    
-    seq = re.split(".+\[(.+).+]*\"(A-Z)*(.+) (.+)\" ([0-9]{3})", x)
-    file = seq[2]
-    fname = {}
-    if file in fname:
-        fname[file] += 1
-    else:
-        fname[file] = 1
-    
+    perc2 = "{:.0%}".format(decc)                       #convert to percent
+        
 
-print("unsuccessful requests = ",perc)
+# for questions 3 and 4
+print("unsuccessful requests = ",perc)                  # I feel like my numbers are wrong. Please double check for both
 print("redirected requests = ",perc2)
-# print("Least requested: " + str(list(fname.keys())[-1]))
 
 
+# for questions 5 & 6
+sorted_d = dict( sorted(records.items(), key=operator.itemgetter(1),reverse=True))        # descending order
 
-# I feel like my numbers are wrong :( but the code works
+print("Most requested: " + str(list(records.keys())[0]))
+print("Least requested: " + str(list(records.keys())[-1]))
