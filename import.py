8e3a2a66780b8cb6355cfeff199019418bcc3cb8
krislincoln13 <123654898+krislincoln13@users.noreply.github.com>
2023-02-14 15:09:58 -0600

    comments

--- import.py
+++ import.py
@@ -1,13 +1,14 @@
 import urllib.request
 
-textfile = urllib.request.urlopen("https://s3.amazonaws.com/tcmg476/http_access_log")
+try:
+    textfile = urllib.request.urlopen("https://s3.amazonaws.com/tcmg476/http_access_log")
+except urllib.error.URLError as e:
+    print("Failed to connect to the URL: ", e)
+    exit()
 
-data = open('EntireLog.txt','w+')
+with open('EntireLog.txt', 'w+', encoding='utf-8') as data:
+    for line in textfile:
+        strline = str(line, encoding='utf-8')
+        data.write(strline + '\n')
 
-for line in textfile:
-    strline = str(line)
-    data.write(strline + '\n')
-    
-
-data.close()
 
