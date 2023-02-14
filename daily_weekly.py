fa47e56a98cfddc166ab35ba79b19f74e6f5cf4e
Austin John <austinjohn@tamu.edu>
2023-02-13 21:01:38 -0600

    start to build weekly script

--- daily_weekly.py
+++ daily_weekly.py
@@ -23,14 +23,8 @@ for i in matches:
 
 print("There were ", x, " requests made on ", ui)
 
-print( "Please enter the first 3 letters of the month you'd like to view data for. For example, January = Jan, February = Feb, etc.")
-print( "If you are attempting to view data for October, please specify the year as follows: Oct/1994 or Oct/1995")
-month_ui = input(" Month: ")
-
-z = 0
-for y in matches:
-    if y.find(month_ui) != -1:
-        z = z + 1
-    
-print ("There were ", z, "requests made in the month:", month_ui)
+firstweek = ["01/", "02/", "03/", "04/", "05/", "06/", "07/"]
+secondweek = []
+thirdweek = []
+fourthweek = []
 
