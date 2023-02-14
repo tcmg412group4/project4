9335f08a6e95dfd619da7a0b0c17e66563a787ef
Austin John <austinjohn@tamu.edu>
2023-02-14 09:22:49 -0600

    week-by-week

--- daily_weekly.py
+++ daily_weekly.py
@@ -24,7 +24,11 @@ for i in matches:
 print("There were ", x, " requests made on ", ui)
 
 firstweek = ["01/", "02/", "03/", "04/", "05/", "06/", "07/"]
-secondweek = []
-thirdweek = []
-fourthweek = []
+secondweek = ["08/", "09/", "10/", "11/", "12/", "13/", "14/"]
+thirdweek = ["15/", "16/", "17/", "18/", "19/", "20/", "21/"]
+fourthweek = ["22/", "23/", "24/", "25/", "26/", "27/", "28/", "29/", "30/", "31/"]
+
+print("Next, the total requests made on a week-by-week basis will be printed out: \n")
+
+
 
