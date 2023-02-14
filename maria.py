ba8b84c664034eb971228dd9f3c11c9ef6cfa566
mariasuchil <mariasuchil_17@tamu.edu>
2023-02-13 18:28:58 -0600

    edited some stuff

--- maria.py
+++ maria.py
@@ -17,9 +17,9 @@ with open('EntireLog.txt') as data:
             bad += 1
         if '302 -'in x:                                 #search for redirects
             good += 1
-        seq = re.split("[A-Z]{3,5} (.+) HTTP/1.0", x)           #pattern to filter out the name of the file only
+        seq = re.split("[A-Z]{3,5} (.+) HTTP/1.0", x)           #pattern to filter out the 'GET..."
         if len(seq) == 3:
-            file = seq[1]                               #index
+            file = seq[1]                               #index the names of the files
             if file in records:                         #count and store it in a dictionary
                 records[file] += 1
             else:
@@ -38,6 +38,5 @@ print("redirected requests = ",perc2)
 
 # for questions 5 & 6
 sorted_d = dict( sorted(records.items(), key=operator.itemgetter(1),reverse=True))        # descending order
-
-print("Most requested: " + str(list(records.keys())[0]))
-print("Least requested: " + str(list(records.keys())[-1]))
+print(("Most requested: " + list(sorted_d.keys())[0]))                                           #turn to list, index the 1st key
+print("Least requested: " + list(sorted_d.keys())[-1])                                  #turn to list, index the last key
